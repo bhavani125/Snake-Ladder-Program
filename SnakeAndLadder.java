db918e4b37c9ef0c2c8e5a49e4ef1e90348cc136
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 20:11:32 +0530

    Adding code to display player position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,5 +3,8 @@ public class SnakeAndLadder
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Snake And Ladder Simulation Program");
+		// Local variable
+		int playerPosition = 0;	
+		System.out.println("Starting position of a single player is " + playerPosition );
 	}
 }
