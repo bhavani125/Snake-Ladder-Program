7a7aeca152666e6640b8548c36732f3a69ce82c6
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 21:34:48 +0530

    added code of checking noplay, ladder, snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
 public class SnakeAndLadder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Snake And Ladder Simulation Program");
 		// Local variable
 		int playerPosition = 0;	
-        int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.print("Enter player name: ");
+		Scanner s = new Scanner(System.in);
+		String playerName = s.next();
+		System.out.println("Player name: " + playerName);
+		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		System.out.println("Dice roll: " + die);
+		
+		int option = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (option) {
+			case 0:
+				System.out.println("No play");
+				playerPosition += 0;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				playerPosition += die;
+				break;
+			default:
+				System.out.println("Snake");
+				playerPosition -= die;
+				if (playerPosition < 0)
+					playerPosition = 0;		
+		}
+		System.out.println("Player position: " + playerPosition);
 	}
-}	
-	
+}
