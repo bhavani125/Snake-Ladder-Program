f9964b9c9274bd98d496c585004edc8a002c7c1c
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 22:06:42 +0530

    added code for reaching winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 public class SnakeAndLadder
 {
+	// Constant 
+	public static final int WinningPosition = 100;
 	public static void main(String[] args)
 	{
 		// Local variable
-		int playerPosition = 0;	
+		int playerPosition = 0;
 		System.out.print("Enter player name: ");
 		Scanner s = new Scanner(System.in);
 		String playerName = s.next();
 		System.out.println("Player name: " + playerName);
-		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("Dice roll: " + die);
-		
-		int option = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (option) {
-			case 0:
-				System.out.println("No play");
+		while ( playerPosition < WinningPostion )
+    	{
+	    	int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		    int option = (int) (Math.floor(Math.random() * 10) % 3);
+		    switch (option) 
+			{
+			  case 0:
+				System.out.println("No play : " + 0);
 				playerPosition += 0;
 				break;
-			case 1:
-				System.out.println("Ladder");
+			  case 1:
+				System.out.println("Ladder  : " + "+" + die);
 				playerPosition += die;
 				break;
-			default:
-				System.out.println("Snake");
+			  default:
+				System.out.println("Snake   : " + "-" + die);
 				playerPosition -= die;
 				if (playerPosition < 0)
-					playerPosition = 0;		
+					playerPosition = 0;
+		    }
 		}
 		System.out.println("Player position: " + playerPosition);
 	}
